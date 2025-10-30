a40140305761139f31d37c6cff553caf5be9e379
Taewoo <nocdon12@naver.com>
2025-10-30 17:32:16 +0900

    🚀프로젝트 완성

--- src/main/kotlin/kr/ac/kumoh/s20220052/w25w07_controller/service/SongService.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220052/w25w07_controller/service/SongService.kt
@@ -1,13 +1,12 @@
 package kr.ac.kumoh.s20220052.w25w07_controller.service
 
 import kr.ac.kumoh.s20220052.w25w07_controller.model.Song
+import kr.ac.kumoh.s20220052.w25w07_controller.repository.SongRepository
 import org.springframework.stereotype.Service
 
 @Service
-class SongService {
-    fun getAllSongs(): List<Song>{
-        return listOf(
-            Song(id = 1, title = "Golden", singer="Hunter/x")
-        )
-    }
+class SongService(
+    private val repository: SongRepository
+) {
+    fun getAllSongs() = repository.findAll()
 }
